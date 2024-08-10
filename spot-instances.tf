61179b4469aba053876a17c40cfbd84d65c38cb7
TYZXNR <163793390+TYZXNR@users.noreply.github.com>
2024-08-09 21:21:02 -0400

    Update spot-instances.tf

--- spot-instances.tf
+++ spot-instances.tf
@@ -32,7 +32,7 @@ resource "aws_spot_instance_request" "ansible-instance" {
 }
 
 # Jenkins Control
-resource "aws_spot_instance_request" "jenkins-instance" {
+resource "aws_spot_instance_request" "jenkins-control" {
   ami             = data.aws_ami.ubuntu_server.id
   instance_type   = "t2.medium"
   spot_price      = "0.94"
@@ -41,7 +41,7 @@ resource "aws_spot_instance_request" "jenkins-instance" {
   subnet_id          = aws_subnet.public-subnet-1.id
   associate_public_ip_address = true
   tags = {
-    Name = "jenkins-instance"
+    Name = "jenkins-control"
   }
 }
 
