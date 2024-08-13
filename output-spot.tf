c279b7ed3f3d2389f1003a6906792326979fc281
TYZXNR <163793390+TYZXNR@users.noreply.github.com>
2024-08-13 17:46:27 -0400

    Update output-spot.tf

--- output-spot.tf
+++ output-spot.tf
@@ -1,6 +1,6 @@
 # Data source to fetch instance details
 data "aws_instance" "jenkins_instance" {
-  instance_id = aws_spot_instance_request.jenkins-instance.spot_instance_id
+  instance_id = aws_spot_instance_request.jenkins-control.spot_instance_id
 }
 
 # Output the public IP address of the Jenkins instance
